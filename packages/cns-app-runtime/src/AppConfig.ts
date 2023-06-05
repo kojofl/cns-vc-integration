8edf0e6adeffd46b9f8e1527c1682ce7f93770bb
Korbinian Flietel <korbinian.flietel@js-soft.com>
2023-06-05 13:37:08 +0200

    init app runtime

---
+++ packages/cns-app-runtime/src/AppConfig.ts
@@ -0,0 +1,90 @@
+import { RuntimeConfig } from "@nmshd/runtime"
+import { IConfigOverwrite, Realm } from "@nmshd/transport"
+import { defaultsDeep } from "lodash"
+
+export interface AppConfig extends RuntimeConfig {
+    logging: any
+    accountsDbName: string
+}
+
+export interface AppConfigOverwrite {
+    transportLibrary?: Omit<IConfigOverwrite, "supportedIdentityVersion">
+    logging?: any
+    accountsDbName?: string
+}
+
+export function createAppConfig(...configs: AppConfigOverwrite[]): AppConfig {
+    const appConfig = {
+        accountsDbName: "accounts",
+        transportLibrary: {
+            realm: Realm.Prod,
+            datawalletEnabled: true
+        },
+        modules: {
+            appLaunch: {
+                name: "appLaunch",
+                displayName: "App Launch Module",
+                location: "appLaunch",
+                enabled: true
+            },
+            pushNotification: {
+                name: "pushNotification",
+                displayName: "Push Notification Module",
+                location: "pushNotification",
+                enabled: true
+            },
+            mailReceived: {
+                name: "mailReceived",
+                displayName: "Mail Received Module",
+                location: "mailReceived",
+                enabled: true
+            },
+            onboardingChangeReceived: {
+                name: "onboardingChangeReceived",
+                displayName: "Onboarding Change Received Module",
+                location: "onboardingChangeReceived",
+                enabled: true
+            },
+            messageReceived: {
+                name: "messageReceived",
+                displayName: "Message Received Module",
+                location: "messageReceived",
+                enabled: true
+            },
+            relationshipChanged: {
+                name: "relationshipChanged",
+                displayName: "Relationship Changed Module",
+                location: "relationshipChanged",
+                enabled: true
+            },
+            relationshipTemplateProcessed: {
+                name: "relationshipTemplateProcessed",
+                displayName: "Relationship Template Processed",
+                location: "relationshipTemplateProcessed",
+                enabled: true
+            },
+            decider: {
+                displayName: "Decider Module",
+                name: "DeciderModule",
+                location: "@nmshd/runtime:DeciderModule",
+                enabled: true
+            },
+            request: {
+                displayName: "Request Module",
+                name: "RequestModule",
+                location: "@nmshd/runtime:RequestModule",
+                enabled: true
+            },
+            attributeListener: {
+                enabled: true,
+                name: "AttributeListenerModule",
+                displayName: "Attribute Listener",
+                location: "@nmshd/runtime:AttributeListenerModule"
+            }
+        }
+    }
+
+    const mergedConfig = defaultsDeep({}, ...configs, appConfig)
+
+    return mergedConfig
+}
