ef2436c0b88a3623dd17cc5e44361c62936c3723
Korbinian Flietel <korbinian.flietel@js-soft.com>
2023-07-02 20:07:40 +0200

    chore: additional tests

--- packages/cns-runtime/test/consumption/vc.test.ts
+++ packages/cns-runtime/test/consumption/vc.test.ts
@@ -57,23 +57,36 @@ describe("VC", () => {
         }
     });
 
+    test("Create verifiable attribute", async function () {
+        const creationResult = await sender.consumption.attributes.createVerifiableAttribute({
+            content: {
+                "@type": "IdentityAttribute",
+                value: {
+                    "@type": "BirthDate",
+                    day: 23,
+                    month: 11,
+                    year: 1998
+                },
+                owner: sender.address
+            },
+            subjectDid: "did:key:sender"
+        });
+
+        expect(creationResult).toBeSuccessful();
+
+        const verificationResult = await sender.consumption.attributes.verifyVerifiableCredential({
+            attribute: creationResult.value.content
+        });
+
+        expect(verificationResult).toBeSuccessful();
+
+        expect(verificationResult.value.success).toBeTruthy();
+    });
+
     test("Request verifiable attribute user story", async function () {
         const identityAttribute = await sender.consumption.attributes.getAttribute({
             id
         });
-
-        const createRequestParams = {
-            peer: recipient.address,
-            content: {
-                items: [
-                    ShareAttributeRequestItem.from({
-                        attribute: identityAttribute.value.content,
-                        sourceAttributeId: identityAttribute.value.id,
-                        mustBeAccepted: true
-                    })
-                ]
-            }
-        };
 
         const params = {
             content: {
@@ -122,8 +135,6 @@ describe("VC", () => {
 
         expect(acceptResult).toBeSuccessful();
 
-        console.log((acceptResult.value.response?.content.items[0] as any).attribute);
-
         await recipient.eventBus.waitForEvent(IncomingRequestStatusChangedEvent, (e) => e.data.newStatus === LocalRequestStatus.Decided);
 
         const validation = await sender.consumption.attributes.verifyVerifiableCredential({
@@ -135,21 +146,3 @@ describe("VC", () => {
         expect(validation.value.success).toBeTruthy();
     });
 });
-
-function buildCredential(data: any, subjectDid: string, publicKey: string) {
-    const now = new Date().toJSON();
-    const issuanceDate = `${now.substring(0, now.length - 5)}Z`;
-    const type = data["@type"];
-    console.log(data);
-    delete data["@type"];
-    const credentialSubject: any = {};
-    credentialSubject[`${type}`] = { ...data };
-    credentialSubject["id"] = subjectDid;
-    return {
-        "@context": ["https://www.w3.org/2018/credentials/v1", "https://www.w3.org/2018/credentials/examples/v1", "https://enmeshed.eu/schema"],
-        type: ["VerifiableCredential"],
-        issuer: `did:key:${publicKey}`,
-        issuanceDate,
-        credentialSubject
-    };
-}
